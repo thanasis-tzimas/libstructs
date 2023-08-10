58dc23d32899901d099e41bd06d727d5853b6515
thanasis-tzimas <thanasistzimas@protonmail.com>
2023-08-10 22:32:56 +0300

    Initial commit of the hashtable library.

---
+++ src/hashtable.c
@@ -0,0 +1,43 @@
+#include "hashtable.h"
+
+unsigned hashtable_hash(unsigned long val, unsigned bits)
+{
+	return val * GOLDEN_RATION >> (64 - bits);
+}
+
+void hashtable_init(struct bucket *ht, unsigned size)
+{
+	unsigned i;
+
+	for (i = 0; i < size; i++)
+		(&ht[i])->first = NULL;
+
+}
+
+void hashtable_add(struct bucket_entry *entry,
+	struct bucket *ht,
+	unsigned long key)
+{
+	struct bucket_entry *first = (&ht[hashtable_hash(key, HASH_BITS(ht))])->first;
+	entry->next = first;
+	if (first)
+		first->pprev = &entry->next;
+	(&ht[hashtable_hash(key, HASH_BITS(ht))])->first = entry;
+	entry->pprev = &(&ht[hashtable_hash(key, HASH_BITS(ht))]);
+}
+
+void hashtable_del(struct bucket_entry *entry)
+{
+	entry->next = NULL;
+	entry0->prev = NULL;
+}
+
+int hashtable_is_empty(struct bucket *ht, unsigned size)
+{
+	unsigned i;
+
+	for (i = 0; i < size; i++)
+		if (!bucket_is_empty(&ht[i]))
+			return -1;
+	return 0;
+}
---
+++ src/hashtable.h
@@ -0,0 +1,73 @@
+#ifndef LIBSTRUCTS_hashtable_h
+#define LIBSTRUCTS_hashtable_h
+
+#include <math.h>
+#include <stddef.h>
+
+struct bucket_entry {
+	struct bucket_entry *next, **pprev;
+};
+
+struct bucket {
+	struct bucket_entry *first;
+};
+
+#define BUCKET_INIT { .first = NULL }
+
+#define DECLARE_HASHTABLE(name, bits) \
+	struct bucket name[1 << (bits)]
+
+#define DEFINE_HASHTABLE(name, bits) \
+	DECLARE_HASHTABLE(name, bits) = \
+		{ [0 ... ((1 << (bits)) - 1] = BUCKET_INIT }
+
+#define ARRAY_SIZE(name) \
+	(sizeof(name)/sizeof(name[0]))
+
+#define HASHTABLE_SIZE(name) (ARRAY_SIZE(name))
+
+#define HASHTABLE_BITS(name) (int)log2(HASHTABLE_SIZE(name))
+
+#define GOLDEN_RATION 0x61c8864680b583eull
+
+#define bucket_is_empty(bucket) \
+	!(bucket->first)
+/*
+ * hashtable_hash - a fast hash function for ints 
+ *  by Nadia Yvette Chambers, IBM
+ * @val: the value to be hashed
+ * @bits: the nth most significant bits to be used on the calculation
+ */
+extern unsigned hashtable_hash(unsigned long val, unsigned bits);
+
+/*
+ * hashtable_init - initialize a hashtable
+ * @ht: the hashstable to be initialized
+ * @size: the hashtable's size
+ */
+extern void hashtable_init(struct bucket *ht, unsigned size);
+
+/*
+ * hashtable_add - add a bucket entry into a bucket in the hashtable
+ * @entry: the entry to be added
+ * @ht: the hashtable that the new entry will be added to
+ * @key: the key that will be hashed and added as an entry in the hashtable
+ */
+extern void hashtable_add(struct bucket_entry *entry,
+	struct bucket *ht,
+	unsigned long key);
+
+/*
+ * hashtable_del - delete an entry from the hashtable
+ * @entry: the bucket entry to be deleted
+ */
+extern void hashtable_del(struct bucket_entry *entry);
+
+/*
+ * hashtable_is_empty - test if hashtable is empty
+ * @ht: the hashtable to be tested if empty
+ * @size: the hashtable's size
+ */
+extern int hashtable_is_empty(struct bucket *ht, unsigned size);
+
+#endif
