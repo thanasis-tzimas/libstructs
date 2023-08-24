fb6643933a1b480b6945a1ebc45509d201f19441
thanasis-tzimas <thanasistzimas00@gmail.com>
2023-08-24 11:05:24 +0300

    Extended buckets' functionality.

--- src/bucket.c
+++ src/bucket.c
@@ -1,5 +1,47 @@
 #include "bucket.h"
 
+void bucket_add(struct bucket_entry *new,
+		struct bucket *bucket)
+{
+	struct bucket_entry *first = bucket->first;
+	new->next = first;
+	if (first)
+		first->pprev = &new->next;
+	bucket->first = new;
+	new->pprev = &bucket->first;
+}
+
+void bucket_add_before(struct bucket_entry *new,
+		       struct bucket_entry *next)
+{
+	new->pprev = next->pprev;
+	new->next = next;
+	next->pprev = &new->next;
+	*(new->pprev) = new;
+}
+
+void bucket_add_after(struct bucket_entry *new,
+		      struct bucket_entry *prev)
+{
+	new->next = prev->next;
+	prev->next = new;
+	new->pprev = &prev->next;
+	if (new->next)
+		new->next->pprev = &new->next;
+}
+
+void bucket_del(struct bucket_entry *entry)
+{
+	struct bucket_entry *next = entry->next;
+	struct bucket_entry **pprev = entry->pprev;
+
+	*pprev = next;
+	if (next)
+		next->pprev = pprev;
+	next = NULL;
+	pprev = NULL;
+}
+
 int bucket_is_empty(struct bucket *bucket)
 {
 	return !(bucket->first);
--- src/bucket.h
+++ src/bucket.h
@@ -1,6 +1,8 @@
 #ifndef LIBSTRUCTS_bucket_h
 #define LIBSTRUCTS_bucket_h
 
+#include <stddef.h>
+
 struct bucket {
 	struct bucket_entry *first;
 };
@@ -8,6 +10,36 @@ struct bucket {
 struct bucket_entry {
 	struct bucket_entry *next, **pprev;
 };
+
+/*
+ * bucket_add - add an entry into a bucket
+ * @new: the new entry
+ * @bucket: the bucket that will accommodate the new entry
+ */
+extern void bucket_add(struct bucket_entry *new,
+		       struct bucket *bucket);
+
+/*
+ * bucket_add_before - add an entry before an other entry
+ * @new: the new entry
+ * @next: the entry next to the new one
+ */
+extern void bucket_add_before(struct bucket_entry *new,
+			      struct bucket_entry *next);
+
+/*
+ * bucket_add_after - add an entry after an other entry
+ * @new: the new entry
+ * @prev: the entry previous to the new one
+ */
+extern void bucket_add_after(struct bucket_entry *new,
+			     struct bucket_entry *prev);
+
+/*
+ * bucket_del - delete a bucket from a bucket
+ * @entry: the deleted bucket entry
+ */
+extern void bucket_del(struct bucket_entry *entry);
 
 /*
  * bucket_is_empty - test if bucket has no entries
